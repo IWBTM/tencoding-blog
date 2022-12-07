9426fb764211c20a30b202e4865c547d90350386
IWBTM <rnlduadl2000@naver.com>
2022-12-08 08:43:30 +0900

    first commit

---
+++ src/main/java/com/tencoding/blog/controller/ApiController.java
@@ -0,0 +1,15 @@
+package com.tencoding.blog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+	@GetMapping("/get")
+	public String get() {
+		return "test1";
+	}
+}

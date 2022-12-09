022f974a7651f526cf745062c9a3574ec4223da8
IWBTM <rnlduadl2000@naver.com>
2022-12-09 11:21:12 +0900

    update 및 더티 체킹 사용

--- src/main/java/com/tencoding/blog/controller/DummyControllerTest.java
+++ src/main/java/com/tencoding/blog/controller/DummyControllerTest.java
@@ -2,6 +2,8 @@ package com.tencoding.blog.controller;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +12,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +31,7 @@ public class DummyControllerTest {
 	@Autowired // 순환 참조 조심 TODO!!
 	private IUserRepository iUserRepository;
 
+	// 회원가입
 	// http://localhost:9090/blog/dummy/signup
 	@PostMapping("/signup")
 	public String signUp(@RequestBody User user) {
@@ -40,6 +44,7 @@ public class DummyControllerTest {
 		return "회원가입이 완료 되었습니다.";
 	}
 
+	// 아이디로 회원 정보 가져오기
 	@GetMapping("/user/{id}")
 	public User detail(@PathVariable int id) throws IllegalAccessException {
 //		User user = iUserRepository.findById(id).orElseGet(() -> {
@@ -53,15 +58,35 @@ public class DummyControllerTest {
 		return user;
 	}
 
+	// 모든 회원 정보 가져오기
 	@GetMapping("/user")
 	public List<User> list() {
-		return iUserRepository.findAll();
+		return iUserRepository.findAll(); 
 	}
-	
+
+	// 지정된 수 만큼 회원 정보 가져오기
 	@GetMapping("/users")
 	public Page<User> pageList(@PageableDefault(size = 5, sort = "id", direction = Direction.DESC) Pageable pageable) {
 		Page<User> userPage = iUserRepository.findAll(pageable);
 		List<User> users = iUserRepository.findAll(pageable).getContent();
 		return userPage;
+	}
+
+	@Transactional  // javax.transactional
+	@PutMapping("/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User reqUser) {
+		log.info(">>> id : {}, >>> pw : {}, >>> email {}", id, reqUser.getPassword(), reqUser.getEmail());
+		System.out.println("음?");
+		// 해당 id의 사용자가 존재 하는지에 대한 여부 확인
+		// 사용자가 존재한다면 넘겨 받은 데이터를 가공해서 저장처리
+		// 사용자가 존재하지 않는다면 클라이언트에게 잘못된 요청을 알림
+		User user = iUserRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("잘못된 요청입니다..");
+		});
+
+		user.setPassword(reqUser.getPassword());
+		user.setEmail(reqUser.getEmail());
+//		iUserRepository.save(user);
+		return null;
 	}
 }
